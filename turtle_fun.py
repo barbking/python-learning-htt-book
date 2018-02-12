f4d2f7624a4ad8add46a621f25385c2caed5363b
Barb King <barbarak98@gmail.com>
2018-02-11 20:02:28 -0600

    turtle graphics

--- turtle_fun.py
+++ turtle_fun.py
@@ -11,3 +11,21 @@ while True:
         break
 end_fill()
 done()
+
+# Barbara King clockface.py
+import turtle
+wn = turtle.Screen()
+# radi = int(input("Enter radius of your clock, looks best if great than 50: "))
+radi = 100
+t = turtle.Turtle()
+t.color("royal blue")
+t.shape("turtle")
+t.stamp()
+t.penup()
+for i in range(12):
+    t.forward(radi)
+    t.stamp()
+    t.setposition(0,0)
+    t.left(30)
+
+wn.exitonclick()
